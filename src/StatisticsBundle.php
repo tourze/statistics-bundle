80190f374ab4d3603ec5559c950b0201cc9ed7f6
tourze-ci <you@example.com>
2025-04-08 09:01:42 +0000

    chore(statistics-bundle): 移除StatisticsBundle中不必要的依赖和权限注解

--- src/StatisticsBundle.php
+++ src/StatisticsBundle.php
@@ -3,17 +3,7 @@
 namespace StatisticsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
-use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 
-#[AsPermission(title: '平台统计')]
-class StatisticsBundle extends Bundle implements BundleDependencyInterface
+class StatisticsBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            \Tourze\DoctrineIndexedBundle\DoctrineIndexedBundle::class => ['all' => true],
-            \AmisBundle\AmisBundle::class => ['all' => true],
-        ];
-    }
 }
